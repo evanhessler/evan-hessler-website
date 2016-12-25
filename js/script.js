7ddf622528ce8943dea7c2ad4e133b3a2740fac2
Evan Hessler <ehessler@lion.lmu.edu>
2016-12-25 00:53:28 -0700

    resized nav icons

--- js/script.js
+++ js/script.js
@@ -4,25 +4,7 @@ $(document).ready(function() {
     } else{
     	$('#logo').attr('src', 'images/logo.svg');
     }
-});
 
-$(window).resize(function() {
-    if (document.documentElement.clientWidth <= 1000) {    
-        $('#logo').attr('src', 'images/logo-short.svg');
-    } else{
-    	$('#logo').attr('src', 'images/logo.svg');
-    }
-}).resize()
-
-
-$(".projects-url").click(function(){
-    document.querySelector('#projects').scrollIntoView({ 
-      behavior: 'smooth' 
-    });
-
-});
-
-$(function(){
     $("#intro").typed({
         strings: ["Hi, I'm Evan."],
         typeSpeed: 15,
@@ -35,4 +17,20 @@ $(function(){
     setTimeout(function(){
         $(".fadein").hide().css({opacity:100}).fadeIn(1000);
     }, 1230);
+
+
+    $(".projects-url").click(function(){
+        document.querySelector('#projects').scrollIntoView({ 
+          behavior: 'smooth' 
+        });
+    });
+
 });
+
+$(window).resize(function() {
+    if (document.documentElement.clientWidth <= 1000) {    
+        $('#logo').attr('src', 'images/logo-short.svg');
+    } else{
+    	$('#logo').attr('src', 'images/logo.svg');
+    }
+}).resize()
